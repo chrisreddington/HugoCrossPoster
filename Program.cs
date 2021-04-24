66190c423b236bd89f26a82a59f46a74ea6e4ed6
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2021-04-24 16:12:59 +0100

    Add getTags functionalit, fix comments

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 
 namespace markdown_converter
@@ -12,12 +13,14 @@ namespace markdown_converter
         static List<string> tags;
         static async Task Main(string[] args)
         {
-            string baseUrl = "https://www.cloudwithchris.com";
+            string protocol = "https";
+            string baseUrl = "www.cloudwithchris.com";
             string fileName = "contributing-to-a-hugo-theme.md";
             string sourceFile = await readFile(fileName);
 
             string replaceUrls = await replaceLocalURLs(sourceFile, baseUrl);
             title = await getTitle(sourceFile);
+            tags = await getTags(sourceFile);
 
             Console.WriteLine($"Title is {title}");
             Console.WriteLine($"{replaceUrls}");
@@ -25,10 +28,16 @@ namespace markdown_converter
             MediumPayload mediumPayload = new MediumPayload(){
                 title = title,
                 content = replaceUrls,
-                canonicalUrl = $"{baseUrl}{fileName}"
+                canonicalUrl = await getCanonicalUrl(protocol, baseUrl, fileName)
             };
 
             sendToMedium(mediumPayload);
+        }
+
+        async static Task<string> getCanonicalUrl(string protocol, string baseUrl, string fileName){
+            string fileNamewithoutExtension = fileName.Replace(".md", "");
+            return new UriBuilder(protocol, baseUrl, -1, fileNamewithoutExtension).ToString();
+
         }
 
         async static Task<string> readFile(string fileName)
@@ -39,12 +48,26 @@ namespace markdown_converter
 
         async static Task<string> getTitle (string fileContents)
         {
-            // Find any strings that match the []() syntax, and do not start with http. That means it's a 
-            // local URL.
-            string pattern = "title: (?<title>.+)";
-
+            // Find the title frontmatter in the YAML
+            string pattern = @"title: (?<title>.+)";
             Match match = Regex.Match(fileContents, pattern);
+
+            // Return the value of the <title> matched group
             return await Task<string>.Run(() => match.Groups["title"].Value);
+        }
+
+        async static Task<List<string>> getTags (string fileContents)
+        {
+            // Find the tags frontmatter in the YAML
+            string pattern = @"tags:\n(?<tags>(\-\s[\w\s]+\n)*)";
+            Match match = Regex.Match(fileContents, pattern);
+
+            //Remove the "- " from the string
+            var temp = await Task<string>.Run(() => match.Groups["tags"].Value);
+            temp = temp.Replace("- ", "");
+
+            //Delimit by the new line character
+            return temp.Split('\n', StringSplitOptions.RemoveEmptyEntries).ToList();
         }
 
         async static Task<string> replaceLocalURLs(string fileContents, string baseUrl)
