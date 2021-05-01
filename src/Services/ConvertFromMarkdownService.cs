da54de7fd39b631fffe1c7bd85e1f1f2962d1c7d
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2021-05-01 19:41:43 +0100

    Adjustments based upon unit tests
    Signed-off-by: Chris Reddington <791642+chrisreddington@users.noreply.github.com>

--- src/Services/ConvertFromMarkdownService.cs
+++ src/Services/ConvertFromMarkdownService.cs
@@ -35,8 +35,7 @@ namespace HugoCrossPoster.Services
       public async Task<string> getFrontmatterProperty (string fileContents, string key)
       {
             // Find the frontmatter with the Key 'key' in the YAML
-            string pattern = $@"{key}:\s(?<{key}>[\-\s[\w\s]+\s?]*)";
-            //string pattern = $@"{key}: ('?""?)*(?<{key}>[\w\-]+)('?""?)*";
+            string pattern = $@"{key}: ('?""?)*(?<{key}>[\w-\s]+)('?""?)*";
           Match match = Regex.Match(fileContents, pattern, RegexOptions.IgnoreCase);
 
           // Return the value of the <key> matched group
@@ -47,12 +46,12 @@ namespace HugoCrossPoster.Services
       public async Task<List<string>> getFrontMatterPropertyList (string fileContents, string key, int count = 10, bool urlize = false)
       {
           // Find the frontmatter for the key in the YAML
-          string pattern = $@"{key}:\s(?<{key}>[\-\s[\w\s]+\s?]*)";
+          string pattern = $@"{key}:\s*(?<{key}>(-\s[\w\s\W]+\s?)*)";
             Match match = Regex.Match(fileContents, pattern, RegexOptions.IgnoreCase);
 
           //Remove the "- " from the string
           var temp = await Task<string>.Run(() => match.Groups[key].Value);
-          temp = temp.Replace("- ", "");
+          temp = temp.Replace("- ", "").Replace("\r\n", "\n");
 
           if (urlize){
               temp = temp.Replace(" ","");
@@ -70,10 +69,15 @@ namespace HugoCrossPoster.Services
 
           // We'll then replace it with the markdown []() syntax, but prepending the baseURL variable
           // in front of the URL.
-          string replacement = $"[$1]({baseUrl}$2)";
+          string replacement = $"[$1]({baseUrl}/$2)";
+
+            string uncleanReturn = Regex.Replace(fileContents, pattern, replacement);
+            string cleanReturn = Regex.Replace(uncleanReturn, "(?<!https?:)/{2,}", "/");
+
+
 
           // Replace the contents throughout the doc and return the result.
-          return await Task<string>.Run(() => Regex.Replace(fileContents, pattern, replacement)); 
+            return await Task<string>.Run(() => cleanReturn); 
       }
 
       public async Task<string> removeFrontMatter(string fileContents)
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 using HugoCrossPoster.Services;
 using System.Linq;
@@ -15,25 +14,187 @@ namespace HugoCrossPoster.Tests
         - tag 2";
 
         [Fact]
-        public async void AssertTagsLengthAndValuesCorrectWhenLowerThanCount()
+        public async void AssertTagsLengthAndValuesAreCorrectWhenCountOfTagsIsLowerThanTakeValue()
         {
+            // Arrange is carried out already
 
+            // Act
             List<string> tags = await markdownService.getFrontMatterPropertyList(exampleMarkdown, "tags");
-            //Assert.Equal(2, (await markdownService.getFrontMatterPropertyList(exampleMarkdown, "tags")).Count());
+
+            // Assert
             Assert.Equal(2, tags.Count());
             Assert.Equal("tag1", tags[0]);
             Assert.Equal("tag 2", tags[1]);
         }
 
-
         [Fact]
-        public async void AssertTagsLengthAndVAluesCorrectWhenCountLessThanTags()
+        public async void AssertTagsLengthAndValuesAreCorrectWhenCountIsLessThanTags()
         {
+            // Arrange is carried out already
 
+            // Act
             List<string> tags = await markdownService.getFrontMatterPropertyList(exampleMarkdown, "tags", 1);
-            //Assert.Equal(2, (await markdownService.getFrontMatterPropertyList(exampleMarkdown, "tags")).Count());
+
+            // Assert
             Assert.Single(tags);
             Assert.Equal("tag1", tags[0]);
+        }
+
+        [Fact]
+        public async void AssertRegexWorksWhenThereAreSpacesAfterPropertyName()
+        {
+            // Arrange
+            string exampleMarkdownWithSpace = @"key: 
+            - tag1
+            - tag 2";
+
+            // Act
+            List<string> tags = await markdownService.getFrontMatterPropertyList(exampleMarkdownWithSpace, "key");
+
+            // Assert
+            Assert.Equal(2, tags.Count());
+            Assert.Equal("tag1", tags[0]);
+            Assert.Equal("tag 2", tags[1]);
+        }
+
+        [Fact]
+        public async void AssertRegExWorksWhenThereAreSpecialSymbolsInName()
+        {
+            // Arrange
+            string exampleMarkdownWithSymbols = @"key: 
+            - cloudwithchris.com
+            - hybrid-cloud";
+
+            // Act
+            List<string> tags = await markdownService.getFrontMatterPropertyList(exampleMarkdownWithSymbols, "key");
+
+            // Assert
+            Assert.Equal(2, tags.Count());
+            Assert.Equal("cloudwithchris.com", tags[0]);
+            Assert.Equal("hybrid-cloud", tags[1]);
+        }
+
+        [Fact]
+        public async void AssertRegExCorrectlyIdentifiesDanglingKey()
+        {
+            // Arrange
+            string exampleMarkdownWithNoValuesInList = @"key:
+            anotherkey:";
+
+            // Act
+            List<string> tags = await markdownService.getFrontMatterPropertyList(exampleMarkdownWithNoValuesInList, "key");
+
+            // Assert
+            Assert.Empty(tags);
+        }
+
+        [Fact]
+        public async void AssertRegExDoesntPickUpStringListOfValues()
+        {
+            // Arrange
+            string exampleMarkdownList = @"key: one, two, three, four";
+
+            // Act
+            List<string> tags = await markdownService.getFrontMatterPropertyList(exampleMarkdownList, "key");
+
+            // Assert
+            Assert.Empty(tags);
+        }
+
+        [Fact]
+        public async void AssetFrontMatterKeyValueIsInsenstiveInGetFrontMatterPropertyList()
+        {
+            // Arrange
+            string exampleMarkdownList = @"KeY: one, two, three, four";
+
+            // Act
+            List<string> tags = await markdownService.getFrontMatterPropertyList(exampleMarkdownList, "key");
+
+            // Assert
+            Assert.Empty(tags);
+        }
+
+        [Fact]
+        public async void AssetFrontMatterKeyValueIsInsenstiveInGetFrontMatterProperty()
+        {
+            // Arrange
+            string exampleMarkdownList = @"Title: My Cool Blog Title";
+
+            // Act
+            string value = await markdownService.getFrontmatterProperty(exampleMarkdownList, "title");
+
+            // Assert
+            Assert.Equal("My Cool Blog Title", value);
+        }
+
+        [Fact]
+        public async void AssertRegExDisplaysPropertyValueCorrectly()
+        {
+            // Arrange
+            string exampleMarkdownList = @"title: My Cool Blog Title";
+
+            // Act
+            string value = await markdownService.getFrontmatterProperty(exampleMarkdownList, "title");
+
+            // Assert
+            Assert.Equal("My Cool Blog Title", value);
+        }
+
+        [Fact]
+        public async void AssertHttpsURLsAreNotChanged()
+        {
+            // Arrange
+            string exampleMarkdownContent = @"This is some content. [Here is a link](https://www.google.com).";
+            string baseURL = "www.cloudwithchris.com";
+
+            // Act
+            string value = await markdownService.replaceLocalURLs(exampleMarkdownContent, baseURL);
+
+            // Assert
+            Assert.Equal("This is some content. [Here is a link](https://www.google.com).", value);
+        }
+
+
+        [Fact]
+        public async void AssertHttpURLsAreNotChanged()
+        {
+            // Arrange
+            string exampleMarkdownContent = @"This is some content. [Here is a link](http://www.google.com).";
+            string baseURL = "www.cloudwithchris.com";
+
+            // Act
+            string value = await markdownService.replaceLocalURLs(exampleMarkdownContent, baseURL);
+
+            // Assert
+            Assert.Equal("This is some content. [Here is a link](http://www.google.com).", value);
+        }
+
+        [Fact]
+        public async void AsserLocalURLsWithForwardSlashAreChanged()
+        {
+            // Arrange
+            string exampleMarkdownContent = @"This is some content. [Here is a link](/blog/post).";
+            string baseURL = "www.cloudwithchris.com";
+
+            // Act
+            string value = await markdownService.replaceLocalURLs(exampleMarkdownContent, baseURL);
+
+            // Assert
+            Assert.Equal($"This is some content. [Here is a link]({baseURL}/blog/post).", value);
+        }
+
+        [Fact]
+        public async void AsserLocalURLsWithoutForwardSlashAreChanged()
+        {
+            // Arrange
+            string exampleMarkdownContent = @"This is some content. [Here is a link](blog/post).";
+            string baseURL = "www.cloudwithchris.com";
+
+            // Act
+            string value = await markdownService.replaceLocalURLs(exampleMarkdownContent, baseURL);
+
+            // Assert
+            Assert.Equal($"This is some content. [Here is a link]({baseURL}/blog/post).", value);
         }
     }
 }
